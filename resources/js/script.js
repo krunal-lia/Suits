96e0a33d243b0a79424491eef05eae0b5fcf580e
krunal-lia <krunallia18@gmail.com>
2018-05-26 14:59:23 +0530

    Initial Commit

---
+++ resources/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $('.js--section-intro').waypoint(function(direction) {
+
+        if(direction == "down") 
+        {
+            $('nav').addClass('sticky');
+        }else {
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset: '60px;'
+    }); 
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+
+});
