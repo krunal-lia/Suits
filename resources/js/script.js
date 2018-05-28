aab3f0a5d69275a99929cda12aa2e8ad97898078
krunal-lia <krunallia18@gmail.com>
2018-05-28 15:49:40 +0530

    Added more jQuery and optimized images

--- resources/js/script.js
+++ resources/js/script.js
@@ -27,4 +27,75 @@ $(document).ready(function() {
         });
       });
 
+      var flag = 0;
+
+      $('.menu,.close').click(function() {
+
+        var nav = $('.main-nav');
+        var icon = $('.menu');
+
+        nav.slideToggle(200);
+
+        if (flag == 0) {
+            $('.menu').hide(1);
+            $('.close').show(1);
+            flag = 1;
+        }else{
+            $('.close').hide(1);
+            $('.menu').show(1);
+            flag = 0;
+        }
+
+      });
+
+      $('.js--wp-1').waypoint(function(direction) {
+
+        $('.js--wp-1').addClass('animate fadeIn');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-2').waypoint(function(direction) {
+
+        $('.js--wp-2').addClass('animate fadeInRight');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-3').waypoint(function(direction) {
+
+        $('.js--wp-3').addClass('animate pulse');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-4').waypoint(function(direction) {
+
+        $('.js--wp-4').addClass('animate fadeIn');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-5').waypoint(function(direction) {
+
+        $('.js--wp-5').addClass('animate fadeIn');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-6').waypoint(function(direction) {
+
+        $('.js--wp-6').addClass('animate fadeIn');
+        },{
+        offset: '50%'
+      });
+
+      $('.js--wp-7').waypoint(function(direction) {
+
+        $('.js--wp-7').addClass('animate fadeInUpBig');
+        },{
+        offset: '75%'
+      });
+      
+
 });
